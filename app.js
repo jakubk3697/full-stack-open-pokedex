d228f7505365601eac11684c4a2b8ca4d113effe
Jakub <jakubkaminski3697@gmail.com>
2025-02-03 01:01:24 +0100

    Try gh action for fly cloud

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("dist"));
+
+// Add version endpoint
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+});
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
